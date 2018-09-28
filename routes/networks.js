c177979d8997384e9aead971c841ae3daa64a884
Eyder Padoan Sant Anna <EYDER@bndes.net>
2018-09-28 12:01:16 -0300

    Putting network object in the request before sending to nested routers The goal is to avoid references to network repository in all routers

--- routes/networks.js
+++ routes/networks.js
@@ -1,27 +1,28 @@
 var express = require('express');
+
 var router = express.Router();
+var usersRouter = require('./users');
 
 var networks = require('../repositories/networks');
 var users = require('../repositories/users');
 
-var usersRouter = require('./users');
 
 
 /* GET /networks */
 router.get('/', function(req, res, next) {
-  res.render('networks', { networks:  networks });
+  res.render('networks/network-index', { networks:  networks });
 });
 
 /* GET /networks/:id */
 router.get('/:networkId', function(req, res, next) { 
   const network = networks.find(network => network.id == req.params.networkId);
   const networkUsers = users.filter(user => user.networkId == network.id);
-  res.render('network', { network: network, users: networkUsers });
+  res.render('networks/network', { network: network, users: networkUsers });
 });
 
-/* METHOD /networks/:id/users */
+/* NESTED ROUTES /networks/:id/users */
 router.use('/:networkId/users', function(req, res, next) { 
-    req.networkId = req.params.networkId;
+    req.network = networks.find(network => network.id == req.params.networkId);
     next();
 }, usersRouter);
 
