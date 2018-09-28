cb7f0e8aac2c0e6153d31ff6d666a2a97d00b009
Eyder Padoan Sant Anna <EYDER@bndes.net>
2018-09-28 11:56:37 -0300

    Using a timestamp as user id when randomuser returns a null id

--- services/generate-random-user.js
+++ services/generate-random-user.js
@@ -4,7 +4,7 @@ function generateRandomUser() {
     return rp.get('https://randomuser.me/api/', { json: true })
         .then(response => {
             return {
-                id: response.results[0].id.value,
+                id: response.results[0].id.value ? response.results[0].id.value : new Date().getTime(), 
                 name: response.results[0].name.first + ' ' + response.results[0].name.last,
                 email: response.results[0].email,
                 picture: response.results[0].picture.thumbnail,
