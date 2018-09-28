3cb153cb8c013b7fb80c16453346b0ebc740f077
Eyder Padoan Sant Anna <EYDER@bndes.net>
2018-09-28 12:05:07 -0300

    Putting user object in the request before sending to nested routers. The goal is to avoid references to user repository in nested routers.
    Reflecting view files organization changes.

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,35 @@
 var express = require('express');
-var router = express.Router();
 
-var networks = require('../repositories/networks');
+var router = express.Router();
+var connectionRequestsRouter = require('./connection-requests');
+
 var users = require('../repositories/users');
 
 var generateRandomUser = require('../services/generate-random-user');
 
 /* POST user */
-router.post('/', function(req, res, next) { 
-  const network = networks.find(network => network.id == req.networkId);
+router.post('/', function(req, res, next) {
   generateRandomUser()
     .then(
       user => {
-        user.networkId = network.id;
+        user.networkId = req.network.id;
         users.push(user);
-        res.redirect('/networks/' + network.id);
+        res.redirect('/networks/' + req.network.id);
       }
     )
     .catch(next);
 });
 
 /* GET user */
-router.get('/:userId', function(req, res, next) { 
-  const network = networks.find(network => network.id == req.networkId);
-  const user = users.find(user => user.networkId == network.id && user.id == req.params.userId);
-  res.render('user', { network: network, user: user });
+router.get('/:userId', function(req, res, next) {
+  const user = users.find(user => user.networkId == req.network.id && user.id == req.params.userId);
+  res.render('users/user', { network: req.network, user: user });
 });
+
+/* NESTED ROUTES /networks/:id/users/:id/connection-requests */
+router.use('/:userId/connection-requests', function(req, res, next) { 
+  req.user = users.find(user => user.networkId == req.network.id && user.id == req.params.userId);  
+  next();
+}, connectionRequestsRouter);
 
 module.exports = router;
