03c1beaa1d8e1769cff3eaac70f4fa25a4c45687
Eyder Padoan <eyderpadoan@gmail.com>
2018-11-10 08:46:37 -0200

    Changed neo4j calls to async await.

--- db/credit-network-graph.js
+++ db/credit-network-graph.js
@@ -10,151 +10,127 @@ class CreditNetworkGraph {
     this.driver = neo4j.driver(url, neo4j.auth.basic(username, password));
   }
 
-  createUser(user) {
+  async createUser(user) {
     const session = this.driver.session();
-    return session
-      .run(`
-        CREATE (user:User {id: {id}, name: {name}, email: {email}, picture: {picture}})
-        RETURN user
-      `, user)
-      .then((result) => {
-        session.close();
-        return result.records[0].get('user').properties;
-      })
-      .catch((error) => {
-        session.close();
-        throw error;
-      });
+    try {
+      const result = await session
+        .run(`
+          CREATE (user:User {id: {id}, name: {name}, email: {email}, picture: {picture}})
+          RETURN user
+        `, user);
+      return result.records[0].get('user').properties;
+    } finally {
+      session.close();
+    }
   }
 
-  getUser(userId) {
+  async getUser(userId) {
     const session = this.driver.session();
-    return session
-      .run(`
-        MATCH (user:User {id: $userId})
-        RETURN user
-      `, { userId })
-      .then((result) => {
-        session.close();
-        return result.records[0].get('user').properties;
-      })
-      .catch((error) => {
-        session.close();
-        throw error;
-      });
+    try {
+      const result = await session
+        .run(`
+          MATCH (user:User {id: $userId})
+          RETURN user
+        `, { userId });
+      return result.records[0].get('user').properties;
+    } finally {
+      session.close();
+    }
   }
 
-  listUsers() {
+  async listUsers() {
     const session = this.driver.session();
-    return session
-      .run(`
-        MATCH (user:User)
-        RETURN user
-      `)
-      .then((result) => {
-        session.close();
-        return result.records.map(record => record.get('user').properties);
-      })
-      .catch((error) => {
-        session.close();
-        throw error;
-      });
+    try {
+      const result = await session
+        .run(`
+          MATCH (user:User)
+          RETURN user
+        `);
+      return result.records.map(record => record.get('user').properties);
+    } finally {
+      session.close();
+    }
   }
 
-  listUsersNotRelatedTo(userId) {
+  async listUsersNotRelatedTo(userId) {
     const session = this.driver.session();
-    return session
-      .run(`
-        MATCH (user:User {id: $userId})
-        MATCH (other:User)
-        WHERE NOT other.id = $userId AND NOT (user)--(other)
-        RETURN other
-        `, { userId })
-      .then((result) => {
-        session.close();
-        return result.records.map(record => record.get('other').properties);
-      })
-      .catch((error) => {
-        session.close();
-        throw error;
-      });
+    try {
+      const result = await session
+        .run(`
+          MATCH (user:User {id: $userId})
+          MATCH (other:User)
+          WHERE NOT other.id = $userId AND NOT (user)--(other)
+          RETURN other
+        `, { userId });
+      return result.records.map(record => record.get('other').properties);
+    } finally {
+      session.close();
+    }
   }
 
-  createConnectionRequest(fromUserId, toUserId, limit) {
+  async createConnectionRequest(fromUserId, toUserId, limit) {
     const session = this.driver.session();
-    return session
-      .run(`
-        MATCH (from:User {id: $fromUserId})
-        MATCH (to:User {id: $toUserId})
-        CREATE (from)-[request:CONNECTION_REQUEST {limit: $limit}]->(to)
-        RETURN request
-      `, { fromUserId, toUserId, limit })
-      .then((result) => {
-        session.close();
-        return result.records[0].get('request').properties;
-      })
-      .catch((error) => {
-        session.close();
-        throw error;
-      });
+    try {
+      const result = await session
+        .run(`
+          MATCH (from:User {id: $fromUserId})
+          MATCH (to:User {id: $toUserId})
+          CREATE (from)-[request:CONNECTION_REQUEST {limit: $limit}]->(to)
+          RETURN request
+        `, { fromUserId, toUserId, limit });
+      return result.records[0].get('request').properties;
+    } finally {
+      session.close();
+    }
   }
 
-  getReceivedConnectionRequests(userId) {
+  async getReceivedConnectionRequests(userId) {
     const session = this.driver.session();
-    return session
-      .run(`
-        MATCH (user:User {id: $userId})<-[request:CONNECTION_REQUEST]-(other:User)
-        RETURN request.limit AS limit, other AS from
-        `, { userId })
-      .then((result) => {
-        session.close();
-        return result.records.map(record => ({
-          limit: record.get('limit'),
-          from: record.get('from').properties,
-        }));
-      })
-      .catch((error) => {
-        session.close();
-        throw error;
-      });
+    try {
+      const result = await session
+        .run(`
+          MATCH (user:User {id: $userId})<-[request:CONNECTION_REQUEST]-(other:User)
+          RETURN request.limit AS limit, other AS from
+        `, { userId });
+      return result.records.map(record => ({
+        limit: record.get('limit'),
+        from: record.get('from').properties,
+      }));
+    } finally {
+      session.close();
+    }
   }
 
-  getSentConnectionRequests(userId) {
+  async getSentConnectionRequests(userId) {
     const session = this.driver.session();
-    return session
-      .run(`
-        MATCH (user:User {id: $userId})-[request:CONNECTION_REQUEST]->(other:User)
-        RETURN request.limit AS limit, other AS to
-        `, { userId })
-      .then((result) => {
-        session.close();
-        return result.records.map(record => ({
-          limit: record.get('limit'),
-          to: record.get('to').properties,
-        }));
-      })
-      .catch((error) => {
-        session.close();
-        throw error;
-      });
+    try {
+      const result = await session
+        .run(`
+          MATCH (user:User {id: $userId})-[request:CONNECTION_REQUEST]->(other:User)
+          RETURN request.limit AS limit, other AS to
+        `, { userId });
+      return result.records.map(record => ({
+        limit: record.get('limit'),
+        to: record.get('to').properties,
+      }));
+    } finally {
+      session.close();
+    }
   }
 
-  getUserConnections(userId) {
+  async getUserConnections(userId) {
     const session = this.driver.session();
-    return session
-      .run(`
-        MATCH (user:User {id: $userId})-[connection:CONNECTION]->(other:User)
-        RETURN
-          {limit: connection.limit, debt:connection.debt, to:other} AS connections
-        `, { userId })
-      .then((result) => {
-        session.close();
-        return result.records.map(record => record.get('connections').properties);
-      })
-      .catch((error) => {
-        session.close();
-        throw error;
-      });
+    try {
+      const result = await session
+        .run(`
+          MATCH (user:User {id: $userId})-[connection:CONNECTION]->(other:User)
+          RETURN
+            {limit: connection.limit, debt:connection.debt, to:other} AS connections
+        `, { userId });
+      return result.records.map(record => record.get('connections').properties);
+    } finally {
+      session.close();
+    }
   }
 }
 
