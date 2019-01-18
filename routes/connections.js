b1d6f26b15f0fe3553f7798c0bb715a455e40208
Eyder <eyderpadoan@gmail.com>
2019-01-18 14:08:16 -0200

    Update connections.js

--- routes/connections.js
+++ routes/connections.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const asyncMiddleware = require('../utils/async-middleware');
-const repository = require('../repositories/connections-repository');
+const connections = require('../repositories/connections-repository');
 
 const router = express.Router();
 
 /* GET */
 router.get('/', asyncMiddleware(async (req, res) => {
-  const connections = await repository.findByUserId(req.user.id);
+  const userConnections = await connections.findByUserId(req.user.id);
   res.render('users/connections/connections-index', {
     user: req.user,
-    connections,
+    connections: userConnections,
   });
 }));
 
